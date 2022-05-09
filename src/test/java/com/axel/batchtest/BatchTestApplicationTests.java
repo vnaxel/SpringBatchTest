cb93946fb9f5a6f1a55cb3fbbf2ba9744281eb00
vnaxel <axel.yvenou@gmail.com>
2022-05-09 18:53:13 +0200

    first commit

---
+++ src/test/java/com/axel/batchtest/BatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.batchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
